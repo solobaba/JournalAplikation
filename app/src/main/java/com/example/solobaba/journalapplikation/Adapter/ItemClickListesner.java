92357eaaf54ec5bd07caef21bbefadf533ae1df5
SOLOBABA <solobaba_odu@yahoo.com>
2018-07-05 06:57:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/solobaba/journalapplikation/Adapter/ItemClickListesner.java
@@ -0,0 +1,7 @@
+package com.example.solobaba.journalapplikation.Adapter;
+
+import android.view.View;
+
+public interface ItemClickListesner {
+    void onClick(View view, int position, boolean isLongClick);
+}
---
+++ app/src/main/java/com/example/solobaba/journalapplikation/model/ToDo.java
@@ -0,0 +1,42 @@
+package com.example.solobaba.journalapplikation.model;
+
+public class ToDo {
+
+    private String id, title, description;
+
+    private ToDo() {
+
+    }
+
+    public ToDo(String id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
